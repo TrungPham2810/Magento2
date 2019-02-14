cd8a09be4fb5cfcc0f665eef2abbd858a120e369
TrungPham2810 <trungphamvan2810@gmail.com>
2019-02-14 17:43:23 +0700

    test with session

--- Magento2/app/code/Inchoo/Helloworld/Block/Helloworld.php
+++ Magento2/app/code/Inchoo/Helloworld/Block/Helloworld.php
@@ -3,6 +3,42 @@ namespace Inchoo\Helloworld\Block;
  
 class Helloworld extends \Magento\Framework\View\Element\Template
 {
+    protected $_catalogSession;
+    protected $_customerSession;
+    protected $_checkoutSession;
+        
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,        
+        \Magento\Catalog\Model\Session $catalogSession,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        array $data = []
+    )
+    {        
+        $this->_catalogSession = $catalogSession;
+        $this->_checkoutSession = $checkoutSession;
+        $this->_customerSession = $customerSession;
+        parent::__construct($context, $data);
+    }
+    public function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+        
+    public function getCatalogSession() 
+    {
+        return $this->_catalogSession;
+    }
+    
+    public function getCustomerSession() 
+    {
+        return $this->_customerSession;
+    }
+    
+    public function getCheckoutSession() 
+    {
+        return $this->_checkoutSession;
+    }
     public function getHelloWorldTxt()
     {
         return 'ok nuon';
