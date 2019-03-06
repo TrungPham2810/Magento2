936a15c72eefcae5680d9fe2b79b9f7e46d5bdff
TrungPham2810 <trungphamvan2810@gmail.com>
2019-03-06 15:34:09 +0700

    apply Argument Replacement

--- app/code/Trung/Currency/Plugin/AfterCurrency2.php
+++ app/code/Trung/Currency/Plugin/AfterCurrency2.php
@@ -1,19 +1,19 @@
 <?php
 namespace Trung\Currency\Plugin;
-class AfterCurrency
+class AfterCurrency2 extends \Trung\Currency\Block\Currency
 {
     public function afterGetRegisterData($subject, $result)
     {
 //        return $this->getTemplate();
 //        return $result;
-        return $subject->getTemplate();
+        return 'after 2';
 
 //        return 'new ok chua';
     }
 
     public function beforeHelloWorld($subject, $ok = "ok nuon hihi")
     {
-        $a = $subject->getTemplate();
+        $a = 'okokok before  2';
         return [$a];
 
     }
@@ -23,7 +23,7 @@ class AfterCurrency
 //        echo 'Calling' . __METHOD__ . ' -- before',"\n";
 //        $result = $procede();
 //        echo 'Calling' . __METHOD__ . ' -- after',"\n";
-        return 'New return value';
+        return '2 around New return value ';
 //        return $result;
     }
 
