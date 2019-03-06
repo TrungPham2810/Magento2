b267e5f6854fb63ba19ee4b8f39fbba7dfca0476
TrungPham2810 <trungphamvan2810@gmail.com>
2019-03-06 14:06:21 +0700

    apply Argument Replacement

--- app/code/Trung/Currency/Block/Currency.php
+++ app/code/Trung/Currency/Block/Currency.php
@@ -7,13 +7,20 @@ class Currency extends Template
 {
     protected $request;
     protected $_registry;
+    public $object1;
+    public $object2;
+
 
     public function __construct(
         Template\Context $context,
         \Magento\Framework\Registry $registry,
         \Magento\Framework\App\Request\Http $request,
+        $object1 = "object1",
+        $object2 = "object2",
         array $data = [])
     {
+        $this->object1 = $object1;
+        $this->object2 = $object2;
         $this->_request = $request;
         $this->_registry = $registry;
         parent::__construct($context, $data);
@@ -27,7 +34,10 @@ class Currency extends Template
     {
         return $ok;
     }
-
+    public function testArgumentReplacement()
+    {
+        return $this->object1.' '.$this->object2;
+    }
     public function getInfo()
     {
 
