02d2c02846246a5ca8cb6374536a15f695805e5f
TrungPham2810 <trungphamvan2810@gmail.com>
2019-03-07 15:38:26 +0700

    Logs all front-end action by use observer event controller_action_predispatch

---
+++ app/code/Practiceobserver/Practiceobserver/Observer/CustomPrice.php
@@ -0,0 +1,33 @@
+<?php
+namespace Practiceobserver\Practiceobserver\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+use \Psr\Log\LoggerInterface;
+use Magento\Framework\Event\Observer;
+
+
+class CustomPrice implements ObserverInterface
+{
+//    protected $_session;
+    protected $logger;
+    protected $request;
+    public function __construct(
+        LoggerInterface $logger,
+        \Magento\Framework\App\Request\Http $request
+    )
+    {
+        $this->logger = $logger;
+        $this->request = $request;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+//        if ($this->_session->isLoggedIn()){
+//            die('true');
+//        }
+        $action     = $this->request->getActionName();
+//        die($action);
+        $this->logger->warn($action);
+    }
+}
---
+++ app/code/Practiceobserver/Practiceobserver/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Practiceobserver_Practiceobserver',
+    __DIR__
+);
